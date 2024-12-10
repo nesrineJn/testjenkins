4b5d6d60f5dd9f7cf22eb62d3e6b2114b6e7a266
nesrineJn <nesrine@winshot.net>
2024-12-10 22:03:26 +0100

    initial commit

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+  public void addcommen(){
+    Command c = new Command();
+        c.add() ;
+    }
+  
+}
---
+++ src/test/java/com/example/Command.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Command {
+
+    public void add() {
+        // // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+}
